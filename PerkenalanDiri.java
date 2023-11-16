42bd006c1f1d7368add311f6dc4c6641696b68b3
alfatirsyafur <150919084+alfatirsyafur@users.noreply.github.com>
2023-11-16 18:22:25 +0700

    Add files via upload

---
+++ PerkenalanDiri.java
@@ -0,0 +1,13 @@
+public class PerkenalanDiri {
+    public static void main(String[] args) {
+        
+        String nama = "alfatir muhammad syafur";
+        String kelas = "kelas reguler (B)";
+        String status = "Mahasiswa";
+
+        System.out.println("Assalamualaikum Nama saya " + nama + ".");
+        System.out.println("saya dari " + kelas + ".");
+        System.out.println("Saya masih seorang " + status + ".");
+        System.out.println("terimakasih!");
+    }
+}
